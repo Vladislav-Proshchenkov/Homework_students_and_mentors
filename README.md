ecefd864aedfc470e5fbaa757858bdcf2d78d548
Vladislav Proshchenkov <vladislove.pro@gmail.com>
2024-03-31 20:26:29 +0300

    Fourth task

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2. First task - задание №1
 3. Second task - задание №2
 4. Third task - задание №3
+5. Fourth task - задание №4
--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,7 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-
+        self.average_grades = 0
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -24,14 +24,17 @@ class Student:
 
     def _gate_average_grade(self):
         for value in self.grades.values():
-            self.grades = round(sum(value) / len(value),1)
-        return self.grades
+            self.average_grades = round(sum(value) / len(value),1)
+        return self.average_grades
 
     def __lt__(self, other):
-        if self.grades < other.grades:
-            print(f'Средний балл {self.grades} < среднего балла {other.grades}')
+        if not isinstance(other, Student):
+            print('Нет такого студента')
+            return
+        if self.average_grades < other.average_grades:
+            print(f'Средний балл {self.average_grades} < среднего балла {other.average_grades}')
         else:
-            print(f'Средний балл {self.grades} > среднего балла {other.grades}')
+            print(f'Средний балл {self.average_grades} > среднего балла {other.average_grades}')
 
 class Mentor:
     def __init__(self, name, surname):
@@ -43,6 +46,7 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+        self.average_grades = 0
 
     def __str__(self):
         return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
@@ -50,14 +54,17 @@ class Lecturer(Mentor):
 
     def _gate_average_grade(self):
         for value in self.grades.values():
-            self.grades = round(sum(value) / len(value), 1)
-        return self.grades
+            self.average_grades = round(sum(value) / len(value), 1)
+        return self.average_grades
 
     def __lt__(self, other):
-        if self.grades < other.grades:
-            print(f'Средний балл {self.grades} < среднего балла {other.grades}')
+        if not isinstance(other, Lecturer):
+            print('Нет такого лектора')
+            return
+        if self.average_grades < other.average_grades:
+            print(f'Средний балл {self.average_grades} < среднего балла {other.average_grades}')
         else:
-            print(f'Средний балл {self.grades} > среднего балла {other.grades}')
+            print(f'Средний балл {self.average_grades} > среднего балла {other.average_grades}')
 
 
 
@@ -77,24 +84,26 @@ class Reviewer(Mentor):
 
 lecturer_1 = Lecturer('Пётр', 'Петров')
 lecturer_1.courses_attached += ['Git']
+lecturer_1.courses_attached += ['Python']
 
 some_lecturer = Lecturer('Some', 'Lecturer')
 some_lecturer.courses_attached += ['Git']
+some_lecturer.courses_attached += ['Python']
 
 student_1 = Student('Вася', 'Иванов', 'м')
 student_1.courses_in_progress += ['Git']
+student_1.courses_in_progress += ['Python']
 student_1.finished_courses += ['Введение в программирование']
-student_1.finished_courses += ['Python']
-student_1.rate_lecturer(lecturer_1, 'Git', 8)
-student_1.rate_lecturer(lecturer_1, 'Git', 7)
+student_1.rate_lecturer(lecturer_1, 'Python', 8)
+student_1.rate_lecturer(lecturer_1, 'Python', 7)
 
 some_student = Student('Ruoy', 'Eman', 'м')
 some_student.courses_in_progress += ['Git']
 some_student.courses_in_progress += ['Python']
 some_student.finished_courses += ['Введение в программирование']
-some_student.rate_lecturer(some_lecturer, 'Git', 8)
-some_student.rate_lecturer(some_lecturer, 'Git', 7)
-some_student.rate_lecturer(some_lecturer, 'Git', 10)
+some_student.rate_lecturer(some_lecturer, 'Python', 8)
+some_student.rate_lecturer(some_lecturer, 'Python', 7)
+some_student.rate_lecturer(some_lecturer, 'Python', 10)
 
 some_reviewer = Reviewer('Some', 'Buddy')
 some_reviewer.courses_attached += ['Python']
@@ -105,9 +114,26 @@ some_reviewer.rate_hw(some_student, 'Python', 7)
 
 reviewer_1 = Reviewer('Иван', 'Сидоров')
 reviewer_1.courses_attached += ['Git']
-reviewer_1.rate_hw(student_1, 'Git', 10)
-reviewer_1.rate_hw(student_1, 'Git', 8)
-reviewer_1.rate_hw(student_1, 'Git', 7)
+reviewer_1.courses_attached += ['Python']
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 8)
+reviewer_1.rate_hw(student_1, 'Python', 7)
+
+students = [some_student, student_1]
+def average_grades_students(students, course):
+    grades_of_student = []
+    for student in students:
+        grades_of_student += (student.grades[course])
+    average_grade_of_students = round(sum(grades_of_student) / len(grades_of_student), 2)
+    return average_grade_of_students
+
+lecturers = [some_lecturer, lecturer_1]
+def average_grades_lecturers(lecturers, course):
+    grades_of_lecturer = []
+    for lecturer in lecturers:
+        grades_of_lecturer += (lecturer.grades[course])
+    average_grade_of_lecturers = round(sum(grades_of_lecturer) / len(grades_of_lecturer), 2)
+    return average_grade_of_lecturers
 
 print('Проверяющие:', end='\n\n')
 print(some_reviewer, end='\n\n')
@@ -122,3 +148,6 @@ print('Сравнение лекторов:')
 some_lecturer.__lt__(lecturer_1)
 print('Сравнение студентов:')
 some_student.__lt__(student_1)
+print()
+print('Средний балл студентов по ДЗ Python:', average_grades_students(students, 'Python'))
+print('Средний балл лекторов по курсу Python:', average_grades_lecturers(lecturers, 'Python'))
